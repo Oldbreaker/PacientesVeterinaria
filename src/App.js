79aa71a5798d8b9a515c6ed1cc2ac39e41bd5f58
Fernando Valenzuela Ramos <amdati5000@gmail.com>
2020-01-07 01:41:39 -0600

    agregado formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./bootstrap.min.css";
 import Header from "./components/Header";
+import Nuevacita from "./components/Nuevacita";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div class="container">
+      <div class="container-fluid">
         <Header titulo="Administrador pacientes veterinaria" />
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <Nuevacita />
+          </div>
+        </div>
       </div>
     );
   }
